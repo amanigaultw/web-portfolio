3cc468e6d4bd849ad888f9adff6c949513d806c8
andrewmanigaultw <andrewmanigaultw@gmail.com>
2025-10-28 21:22:35 -0400

    adjusted two column layout spacing

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
     <div className="min-h-screen bg-slate-900 text-slate-300 relative">
       <MouseGradient />
       
-      <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 md:px-12 md:py-20 lg:px-24 lg:py-0">
-        <div className="lg:flex lg:justify-between lg:gap-4">
+      <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 md:px-12 md:py-20 lg:px-16 lg:py-0">
+        <div className="lg:flex lg:justify-between lg:gap-12">
           <Header />
           
           <main className="pt-24 lg:w-1/2 lg:py-24">
--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
   return (
     <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
       <div>
-        <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">
+        <h1 className="text-2xl font-bold tracking-tight text-slate-200 sm:text-5xl">
           Andrew Manigault, PhD
         </h1>
         <h2 className="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">
