bf30c0652db68382c100ca03b5e2714a36432ec8
andrewmanigaultw <andrewmanigaultw@gmail.com>
2025-10-28 21:13:00 -0400

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,13 +9,13 @@ export default function Header() {
     <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
       <div>
         <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">
-          Dr. Andrew Manigault
+          Andrew Manigault, PhD
         </h1>
         <h2 className="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">
           Software Engineer
         </h2>
         <p className="mt-4 max-w-xs leading-normal">
-          I build engaging, data-driven digital experiences.
+          I apply an empirical, scientific approach to finding efficient solutions to practical problems in software.
         </p>
 
         <Navigation />
