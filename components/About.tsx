4a8ce60a9a1c1cfab0491febfe200d736cdf2553
andrewmanigaultw <andrewmanigaultw@gmail.com>
2025-10-30 10:39:54 -0400

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -9,7 +9,7 @@ export default function About() {
           Coming from a background in health psychology research, I discovered my passion for software while using R to streamline my method for data analysis and manuscript writing. What began as curiosity for coding transformed into a deep appreciation for problem-solving through technology, leading me to pursue a career in software engineering to build meaningful, data-driven solutions.
         </p>
         <p className="mb-4">
-          My main focus these days is developing applications and tools that solve practical problems in support of syndromic surveillance activities for InductiveHealth and the CDC. Much like academic writing, software is a highly iterative process that thrives on strong user feedback and rewards those who are compelled to perpetually edit their work.  I love that my job provides me with the opportunity to frequently explore new tech stacks in support of valuable public health mission—stopping disease through technology.
+          My main focus these days is developing applications and tools that solve practical problems in support of syndromic surveillance activities for InductiveHealth and the CDC. Much like academic writing, software engineering is a highly iterative process that thrives on strong user feedback and rewards those who are compelled to perpetually edit their work.  I love that my job provides me with the opportunity to frequently explore new tech stacks in support of valuable public health mission—stopping disease through technology.
         </p>
         <p>
           Outside of work, I'm usually running, hanging out with my wife, cat and dog, or dabbling in game design/development.
