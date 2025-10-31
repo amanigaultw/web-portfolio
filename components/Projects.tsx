681ac85d28b356162d41959ec4cf7dbe145a0014
andrewmanigaultw <andrewmanigaultw@gmail.com>
2025-10-31 10:43:50 -0400

    typos

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -4,7 +4,7 @@ import ProjectCard from './ProjectCard'
 const projects = [
   {
     title: 'Neural Network from Scratch',
-    description: 'A Kaggle notebook showing how to code a neural network from scratch in R.  I followed along a cool Python tutorial on writing a digit recognizer neural network.  This project helped me gain a lot of clarity on how neural networks leverage back propagation to improve their predictions.',
+    description: 'A Kaggle notebook showing how to code a neural network from scratch in R.  I followed along a cool Python tutorial on writing a digit recognizer neural network.  This project helped me gain clarity on how neural networks leverage back propagation to improve their predictions.',
     skills: ['R', 'Python', 'Kaggle'],
     link: 'https://www.kaggle.com/code/andrewmanigault/samson-zhang-s-neural-network-tutorial-but-in-r'
   },
@@ -16,13 +16,13 @@ const projects = [
   },
   {
     title: 'Transcript Origin Analysis',
-    description: 'An R package to carry out a genomic analysis technique that compares gene expression between a sample and a reference expression profile. This project was a software port of my some older Java code graciously shared by Dr. Steve Cole.  I initially intended to re-write the procedure in R to better understand how it works. Here I learned how to compile and run Java code, how to do multithreading in R, and how to break down a larger program into data "checkpoints" to check the accuracy of my port.',
+    description: 'An R package to carry out a genomic analysis technique that compares gene expression between a sample and a reference expression profile. This project was a software port of some older Java code graciously shared by Dr. Steve Cole.  I initially intended to re-write the procedure in R to better understand how it works. Here I learned how to compile and run Java code, how to do multithreading in R, and how to break down a larger program into data "checkpoints" to check the accuracy of my port.',
     skills: ['R', 'Java'],
     link: 'https://github.com/amanigaultw/toa'
   },
   {
     title: 'Hexpow Mobile Game',
-    description: 'A "connect-3" mobile game made with Unity.  The intent of this project was to familiarize myself with C#, Unity, and the process of releasing apps on the Google Play Store. This project was the start of new hobby (game dev) and taught me that Google will prune apps that are not regularly updated 😊. Forunately, the game is still playable on the browser as a WebGL build.',
+    description: 'A "connect-3" mobile game made with Unity.  The intent of this project was to familiarize myself with C#, Unity, and the process of releasing apps on the Google Play Store. This project was the start of a new hobby (game dev) and taught me that Google will prune apps that are not regularly updated 😊. Forunately, the game is still playable on the browser as a WebGL build.',
     skills: ['C#', 'Unity', 'Google Play Store'],
     link: 'https://play.unity.com/en/games/3f289e29-55eb-4725-8274-c91bb8cd52a2/webgl-builds'
   }
@@ -33,7 +33,7 @@ export default function Projects() {
     <section id="projects" className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24">
       <SectionHeader title="Projects" />
       <p className="mb-12">
-          Most of my professional work requires some level of CDC access and is not linked to my personal github. However, I tried to list some shareable project on this website.
+          Most of my professional work requires some level of CDC access and is not linked to my personal github. However, I tried to list some shareable projects on this website.
       </p>
       <div>
         <ul className="group/list">
