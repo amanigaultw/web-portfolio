d70fc3784b50c4997ae9dcde6d6d01eeb41add6b
andrewmanigaultw <andrewmanigaultw@gmail.com>
2025-10-31 09:31:05 -0400

    added dotted glow background aceternity component

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,10 +3,25 @@
 import { useState, useEffect } from 'react'
 import Navigation from './Navigation'
 import SocialLinks from './SocialLinks'
+import { DottedGlowBackground } from "@/components/ui/dotted-glow-background";
 
 export default function Header() {
   return (
     <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
+      <DottedGlowBackground
+              className="pointer-events-none mask-radial-to-90% mask-radial-at-center hidden lg:block"
+              opacity={1}
+              gap={10}
+              radius={1.6}
+              colorLightVar="--color-neutral-500"
+              glowColorLightVar="--color-neutral-600"
+              colorDarkVar="--color-neutral-500"
+              glowColorDarkVar="--color-sky-800"
+              backgroundOpacity={0}
+              speedMin={0.3}
+              speedMax={1.6}
+              speedScale={1.5}
+            />
       <div>
         <h1 className="text-2xl font-bold tracking-tight text-slate-200 sm:text-5xl">
           Andrew Manigault, PhD
