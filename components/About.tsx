baf11d59439bdfda0d9a59cb720ee7f72d73257a
andrewmanigaultw <andrewmanigaultw@gmail.com>
2025-11-02 14:04:45 -0500

    revised download resume button to open pdf in new tab instead

--- components/About.tsx
+++ components/About.tsx
@@ -1,4 +1,4 @@
-import DownloadButton from './DownloadButton'
+import { Button } from "@/components/ui/moving-border";
 import SectionHeader from './SectionHeader'
 
 export default function About() {
@@ -13,7 +13,7 @@ export default function About() {
           My software engineering approach has been shaped by several key skills honed over a decade of research experience—<i><b>scientific experimentation, writing, and learning</b></i>. User acceptance testing and bug fixing greatly benefit from a scientific mindset—one that involves hypothesis testing and measurement. Similar to academic writing, software engineering is an iterative process that benefits greatly from continuous user feedback and regular revisions. Effective researchers and software engineers tend to learn quickly as they are required to keep up with an ever-evolving literature or technology landscape. My research background strengthens my approach to software engineering—a connection championed by respected engineers, who emphasize the integration of science and software. Indeed, David Farley defines software engineering as the application of an empirical, scientific approach to finding efficient, economic solutions to practical problems in software. 
         </p>
         <p className="mb-4">
-          My main focus these days is developing applications and tools that solve practical problems for InductiveHealth and the Centers for Disease Control and Prevention (CDC). I love that my job provides me with the opportunity to frequently explore new tech stacks in support of a valuable public health mission—stopping disease through technology.
+          My main focus these days is developing applications and tools that solve practical problems for <b>InductiveHealth</b> and the Centers for Disease Control and Prevention (<b>CDC</b>). I love that my job provides me with the opportunity to frequently explore new tech stacks in support of a valuable public health mission—stopping disease through technology.
         </p>
         <p className="mb-4">
           Outside of work, I'm usually running, hanging out with my wife, cat and dog, or experimenting with game design/development.
@@ -22,7 +22,17 @@ export default function About() {
           Please reach out if you have any question or are seeking to collaborate.  Cheers!
         </p>
       </div>
-      <DownloadButton filePath="/docs/Andrew-Manigault-Resume.pdf" fileName="Andrew-Manigault-Resume.pdf" />
+      {/* <DownloadButton filePath="/docs/Andrew-Manigault-Resume.pdf" fileName="Andrew-Manigault-Resume.pdf" /> */}
+      <div className="m-15 flex justify-center text-center">
+        <a href="/docs/Andrew-Manigault-Resume.pdf" target="blank">
+          <Button
+            borderRadius="1.75rem"
+            className="bg-slate-900 text-white border-slate-800 hover:bg-slate-800 hover:scale-105 hover:shadow-lg transition-all duration-300 ease-in-out transform"
+            >
+              View Resume
+          </Button>
+        </a>
+      </div>
     </section>
   )
 }
